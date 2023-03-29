f52d9868f7526cce950f1fa021a3d6657659080d
Daniel Lobo <63163182+Dajolo1995@users.noreply.github.com>
2023-03-29 15:57:40 -0500

    prueba

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
+const dotenv = require ("dotenv")
+const bodyParser = require("body-parser");
+
 
 const prisma = new PrismaClient();
+
+
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API REST con Prisma, Express y Node.js");
